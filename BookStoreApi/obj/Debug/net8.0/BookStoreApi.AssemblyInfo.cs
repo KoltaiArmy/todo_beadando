b67e6e740b0a3af4e2be1af9c1197f6294745f59
Koltai Armand <koltaiarmi@gmail.com>
2024-11-10 16:18:47 +0100

    Web API with JavaScript

--- BookStoreApi/obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
+++ BookStoreApi/obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4172f8cdb66e94f34c70d5caed57fb365e09a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6c3596ec5019fe199bddb32fe8c54e7670abc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
